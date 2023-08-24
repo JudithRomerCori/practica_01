a79c6f4c233fc4bec6eafe243b171ef68bae8bdc
JudithRomerCori <142638700+JudithRomerCori@users.noreply.github.com>
2023-08-23 20:40:57 -0400

    Add files via upload

---
+++ practica_1/Ejericicio_01_01.cpp
@@ -0,0 +1,39 @@
+// Materia: Programación I, Paralelo 1
+
+// Autor: Judith Marisol Romero Cori
+
+// Fecha creación: 15/08/2023
+
+// Fecha modificación: 17/08/2023
+
+// Número de ejericio: 1
+
+// Problema planteado: Área de un triángulo
+
+#include <iostream>
+
+float base;
+float altura;
+float resultado;
+using namespace std;
+
+int main()
+{
+
+cout << "Ingrese la base del triangulo: ";
+
+cin >> base;
+
+cout << "Ingrese la altura del triangulo: ";
+
+cin >> altura;
+
+// La Formula matematica para encontrar el area del triangulo
+
+resultado=(base*altura)/2;
+cout << "El area de su triangulo es: " << resultado << endl;
+
+
+
+
+}
